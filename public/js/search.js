92a17a9e3d1a680e42b7c3561a39efc303d80458
yewon-Noh <yewo2nn16@gmail.com>
2021-12-15 10:03:13 +0900

    1215_오류 수정

--- public/js/search.js
+++ public/js/search.js
@@ -4,7 +4,12 @@ const conn = require('./dbConfig');
 // 검색 수행
 function search(req, res){
     var word = req.body.word;
-    var sql = 'SELECT q.*, ct_name, h_name FROM question q, category ct, hashtag h WHERE q.h_id = h.h_id and q.ct_id = ct.ct_id and h_name = ? or ct_name=?';
+
+    var sql;
+    if(word=='정보처리산업기사')
+        sql = 'SELECT q.*, ct_name, h_name FROM question q, category ct, hashtag h WHERE q.h_id = h.h_id and q.ct_id = ct.ct_id and ct_name=?';
+    else 
+        sql = 'SELECT q.*, ct_name, h_name FROM question q, category ct, hashtag h WHERE q.h_id = h.h_id and q.ct_id = ct.ct_id and h_name = ?';
 
     conn.query(sql, [word, word], function(err, results) {
         if(err){
